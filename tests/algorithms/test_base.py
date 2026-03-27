1770e28824e5e0ae60880531e5583ff8bd3d999d
rasse201 <h.rasmus200@gmail.com>
2026-03-27 18:37:21 +0200

    Add unit tests for algorithm base

---
+++ tests/algorithms/test_base.py
@@ -0,0 +1,18 @@
+import unittest
+from src.algorithms.base import PathfindingAlgorithm
+
+
+class TestPathfindingAlgorithm(unittest.TestCase):
+    def setUp(self):
+        self.algorithm = PathfindingAlgorithm(None)
+
+    def test_initialization(self):
+        self.assertIsInstance(self.algorithm, PathfindingAlgorithm)
+
+    def test_search_step_not_implemented(self):
+        with self.assertRaises(NotImplementedError):
+            self.algorithm.search_step()
+
+    def test_reconstruct_step_not_implemented(self):
+        with self.assertRaises(NotImplementedError):
+            self.algorithm.reconstruct_step()
